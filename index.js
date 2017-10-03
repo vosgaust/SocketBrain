a20eb1c38974d9ad93090764e999d0d2e7eafc40
Edu <edugar91@gmail.com>
2017-10-03 23:12:34 +0200

    Socket server working

---
+++ index.js
@@ -0,0 +1,9 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => res.send('Hello'));
+
+io.on('connection', socket => console.log('a user connected'));
+
+http.listen(3000, () => console.log('listening on *:3000'));
