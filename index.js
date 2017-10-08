805e7415386359e6a8105fde4f58d30cb6ad8839
Edu <edugar91@gmail.com>
2017-10-08 20:15:03 +0200

    Joining to rooms and broadcasting working

--- index.js
+++ index.js
@@ -4,6 +4,32 @@ const io = require('socket.io')(http);
 
 app.get('/', (req, res) => res.send('Hello'));
 
-io.on('connection', socket => console.log('a user connected'));
+const _onConnection = function(socket) {
+  const _onJoinRoom = function(payload) {
+    console.log(`User with id: ${payload.userID} has joined to room: ${payload.room}`);
+    socket.join(payload.room);
+  };
+
+  const _onSetValue = function(payload) {
+    io.sockets.in(payload.room).emit('setValue', payload);
+  };
+
+  const _onSyncRequest = function(payload) {
+    console.log('requesting sync')
+    socket.broadcast.emit('syncrequest', payload);
+  };
+
+  const _onSync = function(payload) {
+    socket.broadcast.emit('sync', payload);
+  };
+
+  console.log('A user has connected');
+  socket.on('joinroom', _onJoinRoom);
+  socket.on('setvalue', _onSetValue);
+  socket.on('requestsync', _onSyncRequest);
+  socket.on('sync', _onSync);
+};
+
+io.on('connection', _onConnection);
 
 http.listen(3000, () => console.log('listening on *:3000'));
